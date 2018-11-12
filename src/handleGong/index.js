cb299748692da1ac485e34ccf1afa66a85daa37d
Anna Spysz <anna.spysz@stackery.io>
2018-11-12 11:52:44 -0800

    fixed json

--- src/handleGong/index.js
+++ src/handleGong/index.js
@@ -56,7 +56,7 @@ exports.gongHandler = async event => {
   //   }
   // );
 
-  const githubBody = { message: 'testing' };
+  const githubBody = { 'message': 'testing' };
   request({
       url: process.env.SLACK_WEBHOOK_URL,
       method: 'POST',
