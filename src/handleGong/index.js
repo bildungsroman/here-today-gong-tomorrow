13b422153c4c38fb415e0bf0a951629b522961a4
Anna Spysz <anna.spysz@stackery.io>
2018-11-12 11:48:29 -0800

    request lib

--- src/handleGong/index.js
+++ src/handleGong/index.js
@@ -45,31 +45,31 @@ exports.gongHandler = async event => {
 
   // if the event is an 'update' event, gong the Slack channel!
 
-  request.post(
-    process.env.SLACK_WEBHOOK_URL,
-    { json: { message: 'testing' } },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log('body');
-            console.log(body);
-        }
-    }
-  );
+  // request.post(
+  //   process.env.SLACK_WEBHOOK_URL,
+  //   { json: { message: 'testing' } },
+  //   function (error, response, body) {
+  //       if (!error && response.statusCode == 200) {
+  //           console.log('body');
+  //           console.log(body);
+  //       }
+  //   }
+  // );
 
-  // const githubBody = { message: 'testing' };
-  // request({
-  //     url: process.env.SLACK_WEBHOOK_URL,
-  //     method: 'POST',
-  //     json: true,
-  //     body: githubBody
-  // }, function (error, response, body){
-  //   if (!error && response.statusCode == 200) {
-  //       console.log('body');
-  //       console.log(body);
-  //     } else {
-  //       console.log(error);
-  //     }
-  // });
+  const githubBody = { message: 'testing' };
+  request({
+      url: process.env.SLACK_WEBHOOK_URL,
+      method: 'POST',
+      json: true,
+      body: githubBody
+  }, function (error, response, body){
+    if (!error && response.statusCode == 200) {
+        console.log('body');
+        console.log(body);
+      } else {
+        console.log(error);
+      }
+  });
 
 
   // print some messages to the CloudWatch console (for testing)
